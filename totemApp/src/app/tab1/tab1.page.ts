c5ef5f09cf8c068f385fc69d1f22d4e8317785b5
mariiaclaracm <mariiamendes1006@gmail.com>
2025-04-28 23:32:01 -0300

    botões funcionando

--- totemApp/src/app/tab1/tab1.page.ts
+++ totemApp/src/app/tab1/tab1.page.ts
@@ -10,11 +10,29 @@ import { BidingService } from "../services/biding.service";
 export class Tab1Page {
   isAlertOpen = false;
   alertButtons = ['Action'];
+ 
+  isAlertPrioritariaOpen = false;
+  isAlertGeralOpen = false;
+  isAlertExamesOpen = false;
+  
+    // alertButtons: AlertButton[] = ['OK'];
 
   constructor(
     private bidingService: BidingService
   ) {
     this.bidingService.varivavelTeste = "Shalala";
+  }
+
+  abrirAlertaPrioritaria() {
+    this.isAlertPrioritariaOpen = true;
+  }
+
+  abrirAlertaGeral() {
+    this.isAlertGeralOpen = true;
+  }
+
+  abrirAlertaExames() {
+    this.isAlertExamesOpen = true;
   }
 
   setOpen(isOpen: boolean) {
